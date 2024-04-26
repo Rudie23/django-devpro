776fa5be7d3da12e2344adfde81e803e672b2e6f
Diego <ruan.diego.anjos@outlook.com>
2024-04-26 10:41:21 -0300

    Landing Page

--- website/base/tests/test_home.py
+++ website/base/tests/test_home.py
@@ -1,6 +1,22 @@
+import pytest
 from django.test import Client
+from django.urls import reverse
+from website.django_assertions import assert_contains
 
 
-def test_status_code(client: Client):
-    response = client.get('/')
+@pytest.fixture
+def response(client):
+    resp = client.get(reverse('base:index'))
+    return resp
+
+
+def test_status_code(response):
     assert response.status_code == 200
+
+
+def test_title(response):
+    assert_contains(response, '<title>Bootstrap 5</title>')
+
+
+def test_home_link(response):
+    assert_contains(response, f'<a class="navbar-brand" href="{reverse('base:index')}">Dev Pro</a>')
